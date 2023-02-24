db42c6adb1c601eaf4cf76d027734f3689f8011b
david-lafontant <lafontant.david@gmail.com>
2023-02-23 19:06:04 -0600

    setup display function to display search result

--- index.js
+++ index.js
@@ -1,4 +1,54 @@
-document.getElementById("msg1").innerHTML = "Default";
+document.getElementById("definition").innerHTML;
+
+function display(result) {
+  let signification = "";
+  const word = result[0]["word"];
+  const phonetic = result[0]["phonetic"];
+  const source = result[0]["sourceUrls"];
+  const mp3 = result[0]["phonetics"][0]["audio"];
+  const meanings = result[0]["meanings"];
+  const partOfSpeech = [];
+  const wordsMeanings = [];
+  const synonyms = [];
+
+  for (let item of meanings) {
+    partOfSpeech.push(item["partOfSpeech"]);
+    wordsMeanings.push(item["definitions"]);
+    synonyms.push(item["synonyms"]);
+  }
+
+  signification += `<h1>${word}</h1>`;
+  signification += `<p>${phonetic}</p>`;
+  signification += ` <audio controls >
+   <source src="${mp3}" type="audio/mpeg">
+  Your browser does not support the audio element.
+</audio>`;
+
+  // <audio id="player" src="horse.ogg"></audio>
+
+  // for (let item of wordsMeanings) {
+  //   let value = "";
+  //   for (element of item) {
+  //     value += `<p>${element["definition"]}</p>`;
+  //   }
+  //   return value;
+  // }
+
+  for (let item = 0; item < partOfSpeech.length; item++) {
+    signification += ` <h2><i>${partOfSpeech[item]}</i></h2>`;
+    for (let element of wordsMeanings[item]) {
+      signification += `<p>${element["definition"]}</p>`;
+    }
+    if (item < synonyms.length && synonyms[item].length !== 0) {
+      signification += `<p>Synonym: ${synonyms[item]}</p>`;
+    }
+  }
+  signification += `<p>${source}</p>`;
+  console.log(signification);
+  document.getElementById("definition").innerHTML = signification;
+
+  return;
+}
 
 async function getWord(e) {
   document.getElementById("msg1").innerHTML = "Trigger";
