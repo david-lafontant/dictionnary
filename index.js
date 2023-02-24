1ece3e10baa06ade5bf233d627890db36d5fb37e
david-lafontant <lafontant.david@gmail.com>
2023-02-23 19:14:47 -0600

    Remove unnecessary comment and console.log

--- index.js
+++ index.js
@@ -24,16 +24,6 @@ function display(result) {
   Your browser does not support the audio element.
 </audio>`;
 
-  // <audio id="player" src="horse.ogg"></audio>
-
-  // for (let item of wordsMeanings) {
-  //   let value = "";
-  //   for (element of item) {
-  //     value += `<p>${element["definition"]}</p>`;
-  //   }
-  //   return value;
-  // }
-
   for (let item = 0; item < partOfSpeech.length; item++) {
     signification += ` <h2><i>${partOfSpeech[item]}</i></h2>`;
     for (let element of wordsMeanings[item]) {
@@ -44,27 +34,25 @@ function display(result) {
     }
   }
   signification += `<p>${source}</p>`;
-  console.log(signification);
   document.getElementById("definition").innerHTML = signification;
 
   return;
 }
 
 async function getWord(e) {
-  document.getElementById("msg1").innerHTML = "Trigger";
   const texte = document.getElementById("inputBox");
   var key = e.keyCode || e.which;
   if (key == 13) {
-    // document.getElementById("msg1").innerHTML = texte.value;
     const response = await fetch(
       `https://api.dictionaryapi.dev/api/v2/entries/en/${texte.value}`
     );
     if (!response.ok) {
-      const message = `An error has occured: ${response.status}`;
+      const message = `An error has occured: ${response.message}`;
+      document.getElementById("definition").innerHTML = message;
       throw new Error(message);
     }
-
     const result = await response.json();
     display(result);
+    texte.value = "";
   }
 }
