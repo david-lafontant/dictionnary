cd3a195e20b9958a642904e3d063a3fd0862a2df
david-lafontant <lafontant.david@gmail.com>
2023-03-09 10:12:30 -0600

    Remove comments

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function display(result) {
-  let signification = '';
+  let signification = "";
   const { word } = result[0];
   const { phonetic } = result[0];
   const source = result[0].sourceUrls;
@@ -8,29 +8,6 @@ function display(result) {
   const partOfSpeech = [];
   const wordsMeanings = [];
   const synonyms = [];
-  // console.log(result[0].phonetics.length);
-  // console.log(result[0].phonetics[result[0].phonetics.length-1]["audio"]);
-
-  //   signification += `<h1>${word}</h1>`;
-  //   signification += `<p>${phonetic}</p>`;
-  //   signification += ` <audio controls >
-  //    <source src="${mp3}" type="audio/mpeg">
-  //   Your browser does not support the audio element.
-  // </audio>`;
-
-  //   signification += `<div class="d-flex flex-row justify-content-between align-items-center">
-  //   <div>
-  //     <h1>${word}</h1>
-  //     <p class="purple">${phonetic}</p>
-  //   </div>
-  //   <div>
-  //     <audio controls >
-  //       <source src="${mp3}" type="audio/mpeg">
-  //       Your browser does not support the audio element.
-  //     </audio>
-  //   </div>
-  // </div>
-  // `;
 
   signification += `<div class="d-flex flex-row justify-content-between align-items-center">
 <div>
@@ -60,32 +37,32 @@ function display(result) {
       <p>
       ${element.definition}
       </p>
-      <p>${element.example ? element.example : ''}</p>
+      <p>${element.example ? element.example : ""}</p>
       </li>`;
     }
-    signification += '</ul>';
+    signification += "</ul>";
     if (item < synonyms.length && synonyms[item].length !== 0) {
       signification += `<p><span class="gray">Synonym<span>&emsp; <span class="purple">${synonyms[item]}</span></p>`;
     }
   }
   signification += `<p><span class="gray">Source</span>&emsp;<span class="uderline">${source}</span></p>`;
-  document.getElementById('definition').innerHTML = signification;
+  document.getElementById("definition").innerHTML = signification;
 }
 
 async function getWord(e) {
-  const texte = document.getElementById('inputBox');
+  const texte = document.getElementById("inputBox");
   const key = e.keyCode || e.which;
   if (key === 13) {
     const response = await fetch(
-      `https://api.dictionaryapi.dev/api/v2/entries/en/${texte.value}`,
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${texte.value}`
     );
     if (!response.ok) {
       const message = `An error has occured: ${response.message}`;
-      document.getElementById('definition').innerHTML = message;
+      document.getElementById("definition").innerHTML = message;
       throw new Error(message);
     }
     const result = await response.json();
     display(result);
-    texte.value = '';
+    texte.value = "";
   }
 }
