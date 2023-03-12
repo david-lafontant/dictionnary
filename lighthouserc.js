3a619d7d3a7359c0433fd529eba805b382936518
david-lafontant <lafontant.david@gmail.com>
2023-03-12 08:08:25 -0600

    Fix eslint errors

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,12 +2,12 @@ module.exports = {
   ci: {
     collect: {
       settings: {
-        hostname: "127.0.0.1",
-        chromeFlags: "--no-sandbox  --headless --disable-gpu",
+        hostname: '127.0.0.1',
+        chromeFlags: '--no-sandbox  --headless --disable-gpu',
       },
     },
     upload: {
-      target: "temporary-public-storage",
+      target: 'temporary-public-storage',
     },
   },
 };
