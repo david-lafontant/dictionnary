8f4ee1839f3fb3c218763478d191f64cda5f76c6
david-lafontant <lafontant.david@gmail.com>
2023-03-23 12:06:57 -0600

    Import and fire handleErrors module

--- src/js/getWord.js
+++ src/js/getWord.js
@@ -1,25 +1,20 @@
 import display from "./display.js";
+import handleErrors from "./handleErrors.js";
 
 async function getWord(e) {
   const texte = document.getElementById("inputBox");
-  // const key = e.keyCode || e.which;
-  // if (key === 13) {
   const spinner = document.getElementById("spinner");
   spinner.removeAttribute("hidden");
   const response = await fetch(
     `https://api.dictionaryapi.dev/api/v2/entries/en/${texte.value}`
   );
   if (!response.ok) {
-    spinner.setAttribute("hidden", "");
-    const message = `An error has occured: ${response.message}`;
-    document.getElementById("definition").innerHTML = message;
-    throw new Error(message);
+    handleErrors();
   }
   const result = await response.json();
   display(result);
   spinner.setAttribute("hidden", "");
   texte.value = "";
-  // }
 }
 
 export default getWord;
