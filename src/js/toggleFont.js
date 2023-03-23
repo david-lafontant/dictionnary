d8cff3dc3e4b94f3b0f96a34cc8c022edf6f829a
david-lafontant <lafontant.david@gmail.com>
2023-03-23 11:27:45 -0600

    Create toggleFont module

---
+++ src/js/toggleFont.js
@@ -0,0 +1,13 @@
+function toggleFont(e) {
+  if (e.target.value === "serif") {
+    document.body.style.fontFamily = "serif";
+  }
+  if (e.target.value === "sans-serif") {
+    document.body.style.fontFamily = "sans-serif";
+  }
+  if (e.target.value === "monospace") {
+    document.body.style.fontFamily = "monospace";
+  }
+}
+
+export default toggleFont;
