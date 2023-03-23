03375c7025f5dd7519ea77f2156ed734ddfacab7
david-lafontant <lafontant.david@gmail.com>
2023-03-23 12:06:35 -0600

    Rename module from showSpinner to handleErrors

--- src/js/handleErrors.js
+++ src/js/handleErrors.js
@@ -1,8 +1,8 @@
-function showSpinner() {
+function handleErrors() {
   spinner.setAttribute("hidden", "");
   const message = `An error has occured: ${response.message}`;
   document.getElementById("definition").innerHTML = message;
   throw new Error(message);
 }
 
-export default showSpinner;
+export default handleErrors;
